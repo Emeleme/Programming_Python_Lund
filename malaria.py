2d3614d2f30dcdb7e5cb4248703f93ea196b85d6
Emeleme <ml.mahecha10@uniandes.edu.co>
2023-10-06 12:36:36 +0200

    Back up on the go

--- malaria.py
+++ malaria.py
@@ -12,3 +12,51 @@
 #Version:Python3.0 or more
 
 ###Libraries### 
+import sys
+
+
+###Comands###
+#Opening files
+fasta_file=open("malaria.fna", "r")
+blast_file=open("malaria.blastx.tab", "r")
+new_fasta_file=open("output.txt","w")
+
+#reading files
+line1=fasta_file.readline()
+print(line1)
+print(line1.split())
+for line in fasta_file:
+    print(line)
+    
+
+#For each line in fasta_file search for lines begining with ">" and extract all
+#information until another the next ">" appears and store the result in vasiable
+#named id_seq 
+
+seq_fas=[] #open a variable to store temporarly all the fasta sequences
+new_fasta=[] #open a new variable to store all the modified sequences in fasta format
+
+for line in fasta_file:
+    if line.startswith(">"):
+        if seq_fasta:
+            seq.append(temp); #Keep the previous sequence in seq
+        temp=''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Closing all files
+fasta_file.close()
+blast_file.close()
+new_fasta_file.close()
