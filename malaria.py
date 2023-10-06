091712c2347938fff91e8aade02b7e1ddb9c6e99
Emeleme <ml.mahecha10@uniandes.edu.co>
2023-10-06 11:48:31 +0200

    Initial commit

---
+++ malaria.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+###About###
+
+#Name:malaria.py
+#Author:Maria Laura Mahecha Escobar
+#Description:Script to add the BLAST hit description to the fasta sequence header
+#Institution:Lund University-Programming in python course
+#email:ma5668ma-s@student.lu.se
+#Date:2023y10m06d
+#Version:Python3.0 or more
+
+###Libraries### 
